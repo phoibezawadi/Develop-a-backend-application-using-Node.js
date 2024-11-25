bc98779984c6ef48ab9d2e9cf5017bc1a810408d
bukuru <bukurumbanzarugamba@gmail.com>
2024-11-25 11:54:33 +0200

    angel,phoibe

---
+++ node111.js
@@ -0,0 +1,31 @@
+<html>
+    <body>
+        <script>
+            //phoibe zawadi and uwamahoro angel
+            //string
+            let name="ange";
+            console.log(name);
+
+            //number
+            let a=22;
+            console.log(a);
+
+            //boolean
+            let boolean=true;
+            console.log(boolean);
+
+            //object
+            let myobject={
+                name: "ange",
+                age: 17,
+                whoyouare: "student"
+            };
+            console.log(myobject);
+
+            //array
+            let myarray= [1,2,3,4];
+            console.log(myarray);
+
+        </script>
+    </body>
+</html>
