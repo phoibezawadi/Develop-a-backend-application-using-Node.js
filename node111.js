6ba08bab5e59960d063cade9cede4ff6d3ba0a37
bukuru <bukurumbanzarugamba@gmail.com>
2024-11-26 08:39:28 +0200

    angel $ phoibe

--- node111.js
+++ node111.js
@@ -1,26 +1,21 @@
-<html>
-    <body>
+
         <script>
-            //question 4
-function calculate(operation, num1, num2) {
-  switch (operation) {
-    case 'addition':
-      return num1 + num2;
-    case 'subtraction':
-      return num1 - num2;
-    case 'multiplication':
-      return num1 * num2;
-    default:
-      return 'Invalid operation';
-  }
-}
+            //question 5
+const testArrays = [
+    [1, 2, 3, 4, 5],        // Positive integers
+    [-1, -2, -3, -4, -5],   // Negative integers
+    [0, 0, 0, 0],           // Array of zeros
+    [1.1, 2.2, 3.3],        // Decimal numbers
+    [],                     // Empty array
+];
 
-
+testArrays.forEach((array, index) = {
+    console.log(`Test ${index + 1}: Array [${array}] -> Sum: ${sumOfNumbers(array)}`)
+});
 
 
 
 
 
         </script>
-    </body>
-</html>
+   
